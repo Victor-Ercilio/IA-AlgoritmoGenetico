afec9f11c6bdcb0f806e3b69d1cb4733fc2d2d12
Victor Ecilio Akeno Yamane <Victor.Ercilio@hotmail.com>
2025-04-09 17:02:32 -0300

    Add função resultado

--- exemplos/caixeiro_viajante.py
+++ exemplos/caixeiro_viajante.py
@@ -1,6 +1,6 @@
 from random import randrange, random
 from enum import Enum
-import string, itertools, fractions
+import string, itertools, fractions, time
 
 
 class Via(Enum):
@@ -17,7 +17,7 @@ CUSTO = Custo.MINIMIZAR
 VIA = Via.DUPLA
 
 # Máximo 52, exceto 4 que não foi implementado ainda
-TOTAL_CIDADES = 5
+TOTAL_CIDADES = 10
 CIDADES = (string.ascii_uppercase+string.ascii_lowercase)[:TOTAL_CIDADES]
 ORIGEM = CIDADES[0]
 
@@ -67,7 +67,7 @@ class Rotas:
 
         if len(cidades) > 1:
             for i in range(len(cidades)):
-                if random() < taxa:
+                if realizar_operacao(taxa):
                     atual = cidades[i]
                     outras = cidades.replace(cidades[i], '') 
                     index_outra = randrange(len(outras))
@@ -146,6 +146,21 @@ def calcular_custo_entre_cidades(rota: str) -> int:
             return 3
 
 
+def resultado() -> str:
+    if TOTAL_CIDADES == 4:
+        raise NotImplementedError('funcão custo para 4 cidades não implementado')
+    
+    if CUSTO == Custo.MINIMIZAR:
+        if TOTAL_CIDADES == 5 and VIA == Via.DUPLA:
+            index = CIDADES.index(ORIGEM)
+            return ORIGEM+CIDADES[:index:-1]+CIDADES[:index]+ORIGEM
+        
+        index = CIDADES.index(ORIGEM)
+        return CIDADES[index:]+CIDADES[:index]+ORIGEM
+    
+    return ""
+
+
 def calcular_custo_caminho(caminho: str) -> int:
     if len(caminho) < 2:
         raise ValueError('caminho inválido, mínimo 2 cidades')
@@ -190,9 +205,14 @@ def avaliar_rotas(rotas: list[Rotas]):
             if r.nota > maior_nota:
                 maior_nota = r.nota
         
-        for r in rotas:
-            r.nota = (r.nota * 10.0) / maior_nota
-            total += r.nota
+        if maior_nota == 0:
+            for r in rotas:
+                    r.nota = 10.0
+        else:
+            for r in rotas:
+                r.nota = (r.nota * 10.0) / maior_nota
+        
+        total = sum([ r.nota for r in rotas ])
         
         for r in rotas:
             r.nota_prop = r.nota / total
@@ -225,14 +245,20 @@ def crossover(rota_1: Rotas, rota_2: Rotas, corte: int=None) -> tuple[Rotas, Rot
     fim2 = rota_2.rota[corte:]
 
     if set(fim1) ^ set(fim2):
-        cidades_dif = set(fim1) ^ set(fim2)
-        set_i2f1, set_i1f2 = set(ini2) & set(fim1) & cidades_dif, set(ini1) & set(fim2) & cidades_dif
-        set_dif_i2f1, set_dif_i1f2 = set_i2f1 ^ cidades_dif, set_i1f2 ^ cidades_dif
+        
+        cidades_sobressalentes = set(fim1) ^ set(fim2)
 
-        while len(set_i2f1) > 0:
-            ini2 = ini2.replace(set_i2f1.pop(), set_dif_i2f1.pop()) 
-        while len(set_i1f2) > 0:
-            ini1 = ini1.replace(set_i1f2.pop(), set_dif_i1f2.pop())
+        cidades_a_substituir_i2f1 = set(ini2) & set(fim1) & cidades_sobressalentes
+        cidades_a_substituir_i1f2 = set(ini1) & set(fim2) & cidades_sobressalentes
+        
+        cidades_substitutas_i2f1 = cidades_a_substituir_i2f1 ^ cidades_sobressalentes
+        cidades_substitutas_i1f2 = cidades_a_substituir_i1f2 ^ cidades_sobressalentes
+
+        while len(cidades_a_substituir_i2f1) > 0:
+            ini2 = ini2.replace(cidades_a_substituir_i2f1.pop(), cidades_substitutas_i2f1.pop()) 
+            
+        while len(cidades_a_substituir_i1f2) > 0:
+            ini1 = ini1.replace(cidades_a_substituir_i1f2.pop(), cidades_substitutas_i1f2.pop())
         
     return Rotas(ini1+fim2), Rotas(ini2+fim1)
 
@@ -247,18 +273,28 @@ def criar_prox_geracao(rotas: list[Rotas], taxa_mut: float, taxa_cros: float) ->
     prox_gercao:list[Rotas] = []
 
     # Elitismo - o melhor é copiado direto para próxima geração
-    prox_gercao.append(rotas[0].copy())
+    # prox_gercao.append(rotas[0].copy())
 
     while len(prox_gercao) < tamanho:
         r1, r2 = selecionar_rota(rotas), selecionar_rota(rotas)
-        if random() <= taxa_cros:
+        
+        if realizar_operacao(taxa_cros):
             r1, r2 = crossover(r1, r2)
+        
         r1.aplicar_mutacao(taxa_mut)
         r2.aplicar_mutacao(taxa_mut)
-        prox_gercao.append(r1)
-        prox_gercao.append(r2)
-    
+
+        if (tamanho - len(prox_gercao)) >= 2:
+            prox_gercao.append(r1)
+            prox_gercao.append(r2)
+        else:
+            prox_gercao.append(r1 if realizar_operacao(0.5) else r2)
+
     return prox_gercao
+
+
+def hilight_resultado() -> str:
+    return f'\033[92m{resultado()}\033[00m'
 
 
 def get_cabecalho(tamanho: int) -> str:
@@ -273,12 +309,47 @@ def get_formato_corpo(tamanho: int) -> str:
     return f'\t{{:^{tamanho}}} {{:^{tamanho}.2f}}  {{:^{tamanho}.2%}}  {{:^{tamanho}.2f}}  {{:^{tamanho}.2%}}'
 
 
+def get_cabecalho_melhores(tamanho: int):
+    return f'\t{"Rotas":^{tamanho}}  {"Custo":^{tamanho}}  {"Média Custo":^{tamanho}}'
+
+
+def get_formato_corpo_melhores(tamanho: int):
+    return f'\t{{:^{tamanho}}} {{:^{tamanho}.2f}}  {{:^{tamanho}.2f}}'
+
+
 def exibir_geracao(geracao: list[Rotas]) -> None:
     tam = 16
     forma = get_formato_corpo(tam)
     print(get_cabecalho(tam))
     for r in geracao:
         print(forma.format(r.rota, r.custo, r.custo_prop, r.nota, r.nota_prop))
+
+
+def exibir_geracao_numero(tag: str, atual:int, total: int):
+    """
+    Exibe uma tag fixa e vai atualizando a numeração na mesma 
+    linha de exibição.
+    Utilizar quando a quantidade de gerações é muito grande e
+    não se deseja ver cada individuo de cada geração, mas, sim,
+    visualizar o progresso das gerações (em um terminal).
+    """
+    tam = len(str(total))
+    limpa_num = '\b'*tam + ' '*tam + '\b'*tam
+    
+    tag = f'{tag} '
+    limpa_tag = '\b'*len(tag) + ' '*len(tag) + '\b'*len(tag)
+
+    intervalo = f' de {total}'
+    voltar_intervalo = '\b'*len(intervalo)
+    limpa_intervalo = ' '*len(intervalo) + voltar_intervalo
+
+    if atual == 1:
+        print(f'{tag}{(atual):0>{tam}}{intervalo}', end='', flush=True)
+        print(voltar_intervalo, end='', flush=True)
+    elif atual == total:
+        print(f'{limpa_intervalo}{limpa_num}{limpa_tag}', end='\n', flush=True)
+    else:
+        print(f'{limpa_num}{(atual):0>{tam}}', end='', flush=True)
 
 
 def exibir_problema(taxa_mutacao:float, taxa_crossover:float, total_rotas:int, geracoes:int) -> None:
@@ -292,22 +363,24 @@ def exibir_problema(taxa_mutacao:float, taxa_crossover:float, total_rotas:int, g
     Gerações: {geracoes}
     Taxa mutação: {taxa_mutacao:.2%}
     Taxa crossover: {taxa_crossover:.2%}
-    Quantidade de rotas por geração: {total_rotas+1}
+    Quantidade de rotas por geração: {total_rotas}
 
     """)
 
 
-def exibir_melhores_resultados(melhores: list[tuple[int, Rotas]]) -> None:
+def exibir_melhores_resultados(melhores: list[tuple[int, Rotas, float]]) -> None:
     tam = 16
-    forma = get_formato_corpo(tam)
+    forma = get_formato_corpo_melhores(tam)
     linhas = ''
     geracao = '\tGeração'
     for i in melhores:
-        linhas += f'{geracao:>{len(geracao)}} {i[0]}  {forma.format(i[1].rota, i[1].custo, i[1].custo_prop, i[1].nota, i[1].nota_prop)}\n'
+        linhas += f'{geracao:>{len(geracao)}} {i[0]}  {forma.format(i[1].rota, i[1].custo, i[2])}\n'
 
     print(f"""
+    Solução: {hilight_resultado()}  Custo: {calcular_custo_caminho(resultado())}
+
     Melhores Resultados
-    {"":{len(geracao)}}\t{get_cabecalho(tam)}
+    {"":{len(geracao)}}\t{get_cabecalho_melhores(tam)}
     {linhas}
     """)
 
@@ -329,12 +402,12 @@ def exibir_custos_todas_rotas_possiveis(rotas: list[Rotas]) -> None:
 
 if __name__ == '__main__':
     try:
-        print('-'*50, 'INICIO DO PROGRAMA', '-'*50)
+        print('-'*40, 'INICIO DO PROGRAMA', '-'*40)
 
-        qtd_rotas: int = 8
-        geracoes: int = 4
+        qtd_rotas: int = 12
+        geracoes: int = 50
         geracao: int = 0
-        taxa_mutacao: float = 0.001
+        taxa_mutacao: float = 0.01
         taxa_crossover: float = 0.7
         melhores_resultados: list[tuple[int, Rotas]] = []
 
@@ -349,13 +422,19 @@ if __name__ == '__main__':
             rotas = criar_prox_geracao(rotas, taxa_mutacao, taxa_crossover)
             avaliar_rotas(rotas)
 
-            melhores_resultados.append((geracao, rotas[0].copy()))
-            print(f'Geração {geracao}')
-            exibir_geracao(rotas)
+            media = sum([r.custo for r in rotas]) / len(rotas)
+            melhores_resultados.append((geracao, rotas[0].copy(), media))
+            if geracoes < 10:
+                print(f'Geração {geracao}')
+                exibir_geracao(rotas)
+            else:
+
+                exibir_geracao_numero('\n\nGeração', geracao, geracoes)
+                time.sleep(0.2)
 
         exibir_melhores_resultados(melhores_resultados)
 
     except Exception as e:
         print(f'Erro: {e}')
     finally:
-        print('-'*50, 'FIM PROGRAMA', '-'*50)
+        print('-'*40, 'FIM PROGRAMA', '-'*40)
